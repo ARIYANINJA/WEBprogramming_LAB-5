d58464ef76f90bcf42d211577c35a92536dcb3a8
ariyaninj otgongerel <ariyaninj@gmail.com>
2021-04-20 23:13:12 +0800

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant = "h2" component = "div"> 
+          NAME AND TITLE
+        </Typography>
       </header>
+      <div className = "App-nav">
+        <ButtonGroup variant="text"
+        size = "large"
+         color="secondary">
+  <Button>OVERVIEW</Button>
+  <Button>FEATURES</Button>
+  <Button>DETAILS</Button>
+  <Button>TECHNOLOGY</Button>
+  <Button>FAQ</Button>
+  </ButtonGroup>
+  <div className = "Add-button">
+        <Button onClick ={()=>alert("YOU CLICKED ADD BUTTON")} variant = "contained" color = "secondary"
+        style = {{borderRadius: 100,
+          fontSize: 30
+        }}>
+           +
+        </Button>
+        </div>
+      </div>                 
     </div>
   );
 }
-
 export default App;
